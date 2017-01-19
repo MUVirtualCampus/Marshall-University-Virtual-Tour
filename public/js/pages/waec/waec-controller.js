a06faa8467aa6ee7a90b85b47d4fd0a85d12c4a0
Drew R. Foglesong <fogleson@marshall.edu>
2017-01-19 10:41:38 -0500

    add api key. maps don't work

--- public/js/pages/waec/waec-controller.js
+++ public/js/pages/waec/waec-controller.js
@@ -3,7 +3,26 @@ export default class WaecController {
   constructor($scope, $state) {
     this.$scope = $scope;
     this.$state = $state;
+    this.initialize();
+
   }
+
+  initialize() {
+    var fenway = {lat: 42.345573, lng: -71.098326};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      center: fenway,
+      zoom: 14
+    });
+    var panorama = new google.maps.StreetViewPanorama(
+        document.getElementById('pano'), {
+          position: fenway,
+          pov: {
+            heading: 34,
+            pitch: 10
+          }
+        });
+    map.setStreetView(panorama);
+    }
 
 }
 WaecController.$inject = ['$scope', '$state'];

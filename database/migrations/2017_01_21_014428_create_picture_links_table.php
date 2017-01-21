ee073e1e28d2bc4eee0f54cf563304fcdf9322e9
Drew R. Foglesong <fogleson@marshall.edu>
2017-01-20 20:53:03 -0500

    Initial migration files

---
+++ database/migrations/2017_01_21_014428_create_picture_links_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePictureLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('picture_links', function (Blueprint $table) {
+          $table->increments('picture_link_id');
+          $table->integer('first_picture_id')->unsigned();
+          $table->integer('second_picture_id')->unsigned();
+          $table->integer('location_id')->unsigned();
+          $table->integer('floor')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('picture_links', function (Blueprint $table) {
+            //
+        });
+    }
+}

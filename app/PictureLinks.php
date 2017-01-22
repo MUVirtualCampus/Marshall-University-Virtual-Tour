4d4d083eb689b136aa8f04681a0cd9a9778ae66d
Drew R. Foglesong <fogleson@marshall.edu>
2017-01-21 19:01:41 -0500

    Add models

---
+++ app/PictureLinks.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PictureLinks extends Model
+{
+    protected $table = 'picture_links';
+    public $timestamps = false;
+}
---
+++ app/PlacesOfInterest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlacesOfInterest extends Model
+{
+    protected $table = 'places_of_interest';
+    public $timestamps = false;
+}

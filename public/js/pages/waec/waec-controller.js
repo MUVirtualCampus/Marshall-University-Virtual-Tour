8cbedac6b7def458604d040550a1416f8af07e14
Drew R. Foglesong <fogleson@marshall.edu>
2017-02-09 21:05:51 -0500

    Programmatically add pano links

--- public/js/pages/waec/waec-controller.js
+++ public/js/pages/waec/waec-controller.js
@@ -31,11 +31,11 @@ export default class WaecController {
     this.pictureLinkService = pictureLinkService;
     this.pictures = pictures;
     this.pictureLinks = pictureLinks;
-    this.initPano();
+    this.initPano(pictures[0]);
 
   }
 
-    initPano() {
+    initPano(picture) {
         // Set up Street View and initially set it visible. Register the
         // custom panorama provider function. Set the StreetView to display
         // the custom panorama 'reception' which we check for below.
@@ -43,83 +43,46 @@ export default class WaecController {
           document.getElementById('pano'), {
             pano: 'waec_3_1',
             visible: true,
-            panoProvider: this.getCustomPanorama
+            panoProvider: () => {return this.getCustomPanorama(panorama.pano);}
         });
       }
 
-      // Return a pano image given the panoID.
-      getCustomPanoramaTileUrl(pano, zoom, tileX, tileY) {
-        // Note: robust custom panorama methods would require tiled pano data.
-        // Here we're just using a single tile, set to the tile size and equal
-        // to the pano "world" size.
-        return 'https://developers.google.com/maps/documentation/javascript/examples/full/images/panoReception1024-0.jpg';
-      }
 
       // Construct the appropriate StreetViewPanoramaData given
-      // the passed pano IDs.
-      getCustomPanorama(pano, zoom, tileX, tileY) {
-        if (pano === 'waec_3_1') {
-          return {
-            location: {
-              pano: 'waec_3_1',
-              description: 'WAEC 3rd Floor'
-            },
-            links: [{
-              heading: 60,
-              description: 'Exit',
-              pano: 'test_image'
-            }],
-            // The text for the copyright control.
-            copyright: '',
-            // The definition of the tiles for this panorama.
-            tiles: {
-              tileSize: new google.maps.Size(1024, 512),
-              worldSize: new google.maps.Size(1024, 512),
-              // The heading in degrees at the origin of the panorama
-              // tile set.
-              centerHeading: 105,
-              getTileUrl: () => {return 'http://res.cloudinary.com/muvirtualcampus/image/upload/c_scale,w_6600/v1486063191/PANO_20170202_125452_2_iu844f.jpg';}
-            }
-          };
-        } else if (pano === 'test_image') {
-          return {
-            location: {
-              pano: 'test_image',
-              description: 'WAEC 3rd Floor'
-            },
-            links: [{
-              heading: 195,
-              description: 'WAEC 3rd Floor',
-              pano: 'waec_3_1'
-            }],
-            // The text for the copyright control.
-            copyright: '',
-            // The definition of the tiles for this panorama.
-            tiles: {
-              tileSize: new google.maps.Size(1024, 512),
-              worldSize: new google.maps.Size(1024, 512),
-              // The heading in degrees at the origin of the panorama
-              // tile set.
-              centerHeading: 105,
-              getTileUrl: () => {return 'http://res.cloudinary.com/muvirtualcampus/image/upload/c_scale,w_6000/v1486063942/PANO_20170202_142958_1_xaua1r.jpg';}
-            }
-          };
-        }
+      // the passed picture.
+      getCustomPanorama(pano) {
+        let picture = _.find(this.pictures, item => item.pano === pano);
+        return {
+          location: {
+            pano: picture.pano,
+            description: picture.description
+          },
+          links: this.setLinks(picture.picture_id),
+          // The text for the copyright control.
+          copyright: '',
+          // The definition of the tiles for this panorama.
+          tiles: {
+            tileSize: new google.maps.Size(1024, 512),
+            worldSize: new google.maps.Size(1024, 512),
+            // The heading in degrees at the origin of the panorama
+            // tile set.
+            centerHeading: picture.heading,
+            getTileUrl: () => { return picture.url; }
+          }
+        };
       }
 
-      setLinks(pictureId) {
+      setLinks(picture_id) {
         let links = [];
-
-        _.forEach(this.pictureLinks), (item) => {
-          if(item.firstPictureId === pictureId) {
-            links.push(
-              _.find(this.pictures, (picture) => {
-                return picture.pictureId === item.secondPictureId;
-              })
-            );
+        _.forEach(this.pictureLinks, (item) => {
+          if(item.first_picture_id === picture_id) {
+            let link =
+              _.find(this.pictures, picture =>
+                picture.picture_id === item.second_picture_id
+              );
+              links.push({heading: item.heading, description: link.description, pano: link.pano});
           }
-        }
-
+        });
         return links;
       }
 

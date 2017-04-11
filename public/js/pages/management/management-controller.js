36ddf55948f58dc0509edcec1ae560908e3ed641
Daniel Lewis <lewis411@live.marshall.edu>
2017-04-11 16:04:19 -0400

    added controller for management page

--- public/js/pages/management/management-controller.js
+++ public/js/pages/management/management-controller.js
@@ -1,10 +1,65 @@
+import _ from 'lodash';
+
 export default class ManagementController {
+  static resolve() {
+    return {
+      locations: ['$stateParams', 'locationService', ($stateParams, locationService) => {
+        return locationService.getAll('locations').then((results) => {
+            return results.data;
+          });
+        }
+
+      ],
+      location: ['$stateParams', 'locations', ($stateParams, locations) => {
+        return _.find(locations, item => item.name === $stateParams.name);
+        }
+      ],
+      pictures: ['$stateParams', 'pictureService', 'location', ($stateParams, pictureService, location) => {
+        return pictureService.getPictures(location.location_id).then((results) => {
+            return results.data;
+          });
+        }
+
+      ],
+      pictureLinks: ['$stateParams', 'pictureLinkService', 'location', ($stateParams, pictureLinkService, location) => {
+        return pictureLinkService.getPictureLinks(location.location_id).then((results) => {
+            return results.data;
+          });
+        }
+      ],
+      placesOfInterest: ['$stateParams', 'placeOfInterestService', 'location', ($stateParams, placeOfInterestService, location) => {
+        return placeOfInterestService.getPlacesOfInterest(location.location_id).then((results) => {
+            return results.data;
+          });
+        }
+      ]
+    }
+  }
+
+  static get $inject(){
+    return ['$scope', '$state', 'locationService', 'pictureService', 'pictureLinkService', 'locations', 'location', 'pictures', 'pictureLinks', 'placesOfInterest'];
+  }
 
   constructor($scope, $state) {
     this.$scope = $scope;
     this.$state = $state;
+    this.locationService = locationService;
+    this.pictureService = pictureService;
+    this.pictureLinkService = pictureLinkService;
+    this.locations = locations;
+    this.location = location;
+    this.pictures = pictures;
+    this.pictureLinks = pictureLinks;
+    this.placesOfInterest = placesOfInterest;
+  }
+
+  switchLocation() {
+  	//add code
+  }
+
+  switchFloor() {
+  	//add code
   }
 
 }
-ManagementController.$inject = ['$scope', '$state'];
-export default ManagementController;
+

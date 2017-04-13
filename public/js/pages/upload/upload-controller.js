1bec75fc1f4ee99b96b98c582f69a64667813f5a
Daniel Lewis <lewis411@live.marshall.edu>
2017-04-13 10:02:41 -0400

    View and routing for picture upload has been created. Controller is work-in-progress.

--- public/js/pages/upload/upload-controller.js
+++ public/js/pages/upload/upload-controller.js
@@ -1,10 +1,20 @@
 export default class UploadController {
 
-  constructor($scope, $state) {
+  constructor($scope, $state, $timeout) {
     this.$scope = $scope;
     this.$state = $state;
+    this.$timeout = $timeout;
+  }
+
+  uploadPanorama(file) {
+    file.upload = Upload.upload({
+      url: 'https://api.imgur.com/3/image',
+      method: 'POST',
+      headers: {'Authorization': 'Client-ID e729cdb81257a9d'},
+      data: {file: file},
+    });
   }
 
 }
-UploadController.$inject = ['$scope', '$state'];
+UploadController.$inject = ['$scope', '$state', '$timeout'];
 export default UploadController;

41f0592a1618fd797832ad501f8557e28594946a
Drew R. Foglesong <fogleson@marshall.edu>
2017-04-19 19:03:15 -0400

    filtering pictures css broken

--- public/js/pages/editor/pictures/picture-controller.js
+++ public/js/pages/editor/pictures/picture-controller.js
@@ -12,11 +12,30 @@ export default class pictureController {
     //TODO: Change all this crap
     this.pictures = _.filter(pictures, (picture) => {return picture.floor === 4});
     this.location = _.find(locations, (location) => {return location.location_id === 1});
+    //TODO: Remove this line and uncomment the other for production
     this.floor = 4;
+    this.floors = this.initFloors();
+    //this.floor = this.floors.length > 0 ? this.floors[0] : null;
   }
 
   goToUpload() {
     this.$state.go('home.editor.upload', {params: {location: this.location.name, floor: this.floor}});
+  }
+
+  switchLocations(){
+    console.log('Switch!');
+  }
+
+  switchFloors() {
+    console.log('Switch!');
+  }
+
+  initFloors() {
+    let floorArray = [];
+    for(let i = 1; i <= this.location.floors; i++) {
+      floorArray.push(i);
+    }
+    return floorArray;
   }
 
 }

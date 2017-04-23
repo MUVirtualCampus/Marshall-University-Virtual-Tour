d03204722ba2a54d3b984108ae99ecc7cacdafff
Zach Litteral <zlitteral@outlook.com>
2017-04-23 18:13:22 -0400

    Help screen added

--- public/js/pages/home-controller.js
+++ public/js/pages/home-controller.js
@@ -13,6 +13,24 @@ class HomeController {
     return this.$state.current.name === 'home.tour';
   }
 
+ showHint(){
+  /*
+  console.log("HEEEEEEEEEEEEEEEEEEEEEEEEEEEYYYYYYYYYYYYYYYYYYY");
+    var currentState = this.$state.current.name;
+    var currentScope = this.$scope;
+    if (currentState == "home.dashboard"){
+      console.log(currentState);
+    }
+    console.log(currentState);
+    console.log(currentSc+ope);
+    return currentState
+*/
+  var location = '../manuals/usersManualPDF';
+  window.open(location,"resizeable,scrollbar"); 
+  //var currentState = this.$state.current.name;
+  console.log("IM IN THIS FUNCTION");
+  return false;
+ }
 
 
 }

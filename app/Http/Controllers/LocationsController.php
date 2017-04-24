165a676aed6ef7d6199f66d70abaac44338d09d8
Daniel Lewis <lewis411@live.marshall.edu>
2017-04-23 21:39:28 -0400

    Added create/update returns for locations toast.

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -25,26 +25,39 @@ class LocationsController extends Controller
 
   public function update(Request $request)
   {
-    $id = $request->input('location_id');
-    $location = Locations::where('location_id', $id)
-                         ->update([                            
-                            'name' => $request->input('name'),
-                            'description' => $request->input('description'),
-                            'floors' => $request->input('floors')
-                          ]);
-    return ['success' => true];
-
+    try
+    {
+      $id = $request->input('location_id');
+      $location = Locations::where('location_id', $id)
+                           ->update([                            
+                              'name' => $request->input('name'),
+                              'description' => $request->input('description'),
+                              'floors' => $request->input('floors')
+                            ]);
+      return ['success' => true];
+    }
+    catch(\Exception $e)
+    {
+      return ['success' => false];
+    }
   }
 
   public function create(Request $request)
   {
-    Locations::create(array(
-        'location_id' => $request->get('location_id'),
-        'name' => $request->get('name'),
-        'description' => $request->get('description'),
-        'floors' => $request->get('floors')
-      ));
+    try
+    {
+      Locations::create(array(
+          'location_id' => $request->get('location_id'),
+          'name' => $request->get('name'),
+          'description' => $request->get('description'),
+          'floors' => $request->get('floors')
+        ));
 
-    return ['success' => true];
+      return ['success' => true];
+    }
+    catch(\Exception $e)
+    {
+      return ['success' => false];
+    }
   }
 }

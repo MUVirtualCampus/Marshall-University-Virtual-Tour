7d2026099f45304b848c0d4cb56bb7bab0079027
Daniel Lewis <lewis411@live.marshall.edu>
2017-04-24 00:03:11 -0400

    remove upload directive.

--- public/js/app-directives.js
+++ public/js/app-directives.js
@@ -3,13 +3,11 @@ import angular from 'angular';
 import pictureEditorModule from './common/directives/picture-editor/picture-editor-module';
 import locationEditorModule from './common/directives/location-editor/location-editor-module';
 import poiEditorModule from './common/directives/poi-editor/poi-editor-module';
-import pictureUploadModule from './common/directives/picture-upload/picture-upload-module';
 
 let dependencies = [
   pictureEditorModule.name,
   locationEditorModule.name,
   poiEditorModule.name,
-  pictureUploadModule.name
 ];
 
 let directivesModule = angular.module('mutour.directives', dependencies)
--- public/js/common/directives/picture-upload/picture-upload-module.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-
-import PictureUploadDirective from './picture-upload-directive';
-
-export default angular.module('mutour.picture-upload-directive', [])
-  .directive(PictureUploadDirective.directiveName, PictureUploadDirective.directiveFactory);

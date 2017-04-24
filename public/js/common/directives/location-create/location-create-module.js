e2ce625a06589686593687398361073b15ae8f3c
Daniel Lewis <lewis411@live.marshall.edu>
2017-04-23 20:16:49 -0400

    removed location-create directive.

--- public/js/common/directives/location-create/location-create-module.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-
-import LocationCreateDirective from './location-create-directive';
-
-export default angular.module('mutour.location-create-directive', [])
-  .directive(LocationCreateDirective.directiveName, LocationCreateDirective.directiveFactory);
